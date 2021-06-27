3da5495fa294e7062548a96a00f4b530542fc0f3
NuBiFoRM <nubiform@me.com>
2021-06-27 14:53:55 +0900

    jpa entity

--- src/main/java/com/nubiform/order/entity/Member.java
+++ src/main/java/com/nubiform/order/entity/Member.java
@@ -1,4 +1,15 @@
 package com.nubiform.order.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "members")
 public class Member {
+
+    @Id
+    @GeneratedValue
+    private long id;
 }
--- src/main/java/com/nubiform/order/entity/OrderDetail.java
+++ src/main/java/com/nubiform/order/entity/OrderDetail.java
@@ -1,4 +1,15 @@
 package com.nubiform.order.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "order_details")
 public class OrderDetail {
+
+    @Id
+    @GeneratedValue
+    private long id;
 }
